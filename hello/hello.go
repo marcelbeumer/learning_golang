dc5d51aded25091d03f9633b5e5685638e2e6857
Marcel Beumer <marcel@marcelbeumer.com>
2022-01-03 16:13:25 +0100

    Tutorial up until https://go.dev/doc/tutorial/random-greeting

---
+++ hello/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"example.com/greetings"
+	"fmt"
+	"log"
+)
+
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	// Get a greeting message and print it.
+	message, err := greetings.Hello("Marcel")
+
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(message)
+}
